6d8b47c89c8451d1e4ed7a8df45eb716594d3190
debize <lucas.debize@epitech.eu>
2023-11-16 11:12:23 +0100

    [fix] responsiveness and routing issue

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,10 +11,10 @@ function Home() {
     <div className='App'>
       <h1 className='lg:ml-[25%] ml-[5%] mt-[2%]'><strong>WELCOME TO THE EPITECH LYON HUB WEBSITE</strong></h1>
       <div className={`flex-box ${ChangeFlexBoxSize}`}>
-        <Link href="/Inventory" className='ml-[5%] mt-[5%] lg:text-3xl md:text-2xl text-lg'><strong>INVENTAIRE</strong></Link>
+        <Link href="/Inventory" className='lg:text-3xl md:text-2xl text-md p-4'><strong>INVENTORY</strong></Link>
       </div>
       <div className="flex-box w-4/5">
-        <Link href="/Project" className='ml-[2%] mt-[2%] lg:text-3xl md:text-2xl text-lg'><strong>PROJETS</strong></Link>
+        <Link href="/Project" className='lg:text-3xl md:text-2xl text-md p-4'><strong>PROJECTS</strong></Link>
       </div>
     </div>
     );
