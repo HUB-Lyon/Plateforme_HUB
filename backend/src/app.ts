c84b6786300b8835818607398aa04d22bf4c9f25
tristan.calard <tristan.calard@epitech.eu>
2023-11-20 11:27:57 +0100

    [feat] swagger :version tuto ok

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,9 +5,11 @@ import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
+import swaggerUi from "swagger-ui-express";
 
 import usersRouter from './routes/users/router_users.js';
 import authRouter from './routes/auth/auth.js';
+import router from "./routes/swagger/router_swagger.js";
 
 dotenv.config({ path: '.env' });
 
@@ -39,10 +41,22 @@ app.use(logger(process.env.NODE_ENV === 'prod' ? 'common' : 'dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
 
 app.use('/inventory', inventoryRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
+app.use(router);
 
 
 app.get('/', (req: Request, res: Response) => {
