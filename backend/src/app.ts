cc07f2e803f03a41f8f7fd6254120efd00f4ddc4
tristan.calard <tristan.calard@epitech.eu>
2023-11-21 10:28:45 +0100

    [add] swagger new method and swagger for auth routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,27 @@ import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
-import swaggerUi from "swagger-ui-express";
 
 import usersRouter from './routes/users/router_users.js';
 import authRouter from './routes/auth/auth.js';
-import router from "./routes/swagger/router_swagger.js";
+
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API',
+    version: '1.0.0',
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  apis: [`dist/routes/**/*.js`],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 dotenv.config({ path: '.env' });
 
@@ -20,8 +36,8 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      httpOnly: true,
-      secure: false,
+    httpOnly: true,
+    secure: false,
     },
   })
 );
@@ -30,21 +46,10 @@ app.use(logger(process.env.NODE_ENV === 'prod' ? 'common' : 'dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("public"));
-
-app.use(
-  "/docs",
-  swaggerUi.serve,
-  swaggerUi.setup(undefined, {
-    swaggerOptions: {
-      url: "/swagger.json",
-    },
-  })
-);
 
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
-app.use(router);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
