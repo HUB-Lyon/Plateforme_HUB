75a26411f1045017fc66a72c70b5a388d3a4f101
debize <lucas.debize@epitech.eu>
2023-11-23 11:55:38 +0100

    [fix] change next-config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // Ajoutez d'autres configurations Next.js si nécessaire
+    target: 'serverless',
+  };
+  
+  module.exports = nextConfig;
+  
