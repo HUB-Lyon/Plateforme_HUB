93f0a15f428eeb0476cc6d44d54b6330e3165cbd
debize <lucas.debize@epitech.eu>
2023-11-29 15:46:05 +0100

    [fix] remove mt replace by gap

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 
 const Home: React.FC = () => {
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center gap-y-20">
             <h1 className='font-bold'>WELCOME TO THE EPITECH LYON HUB WEBSITE</h1>
             <div className="flex-box w-4/5">
                 <Link href="/inventory" className='lg:text-3xl md:text-2xl text-md p-4 font-bold'>INVENTORY</Link>
             </div>
-            <div className="flex-box w-4/5 mb-20">
+            <div className="flex-box w-4/5 gap-y-20">
                 <Link href="/project" className='lg:text-3xl md:text-2xl text-md p-4 font-bold'>PROJECTS</Link>
             </div>
         </div>
