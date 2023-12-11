c5cac83f0de72392d58208f0cff3a9ca7fc55d40
Mortua <laolou.laforest-ravon@epitech.eu>
2023-12-11 11:22:11 +0100

    [fix] : add detail-project + gray-200 -> gray-50 + delete imort node

--- frontend/pages/project.tsx
+++ frontend/pages/project.tsx
@@ -4,25 +4,31 @@ import Link from 'next/link';
 import { fill_project, fill_user } from '../model';
 import Image from 'next/image';
 
+export const getStatus = (status: string): string => {
+    switch (status) {
+    case 'Pending':
+        return 'bg-gray-500';
+    case 'in Progress':
+        return 'bg-yellow-500';
+    case 'Done':
+        return 'bg-green-800';
+    case 'Archived':
+        return 'bg-green-300';
+    case 'Refused':
+        return 'bg-red-500';
+    case 'req finish':
+        return 'bg-purple-500';
+    default:
+        return 'bg-slate-100';
+    }
+};
+export const getTitleStatus = (status: string): string => {
+    if (status === 'Pending' || status === 'in Progress' || status === 'Done' || status === 'Archived' || status ==='Refused' || status === 'req finish')
+        return status;
+    return 'N/A';
+};
+
 const Project: React.FC<{ projects: fill_project[], users: fill_user[] }> = ({ projects, users }) => {
-    const getStatus = (status: string): string => {
-        switch (status) {
-        case 'Pending':
-            return 'bg-gray-500'; 
-        case 'in Progress':
-            return 'bg-yellow-500'; 
-        case 'Done':
-            return 'bg-green-800'; 
-        case 'Archived':
-            return 'bg-green-300'; 
-        case 'Refused':
-            return 'bg-red-500'; 
-        case 'req finish':
-            return 'bg-purple-500'; 
-        default:
-            return 'bg-slate-100';
-        }
-    };
     const getUserInfo = (leaderId: number): fill_user | undefined => {
         return users.find(user => user.id === leaderId);
     };
@@ -34,22 +40,22 @@ const Project: React.FC<{ projects: fill_project[], users: fill_user[] }> = ({ p
                 <Link href="/create-project" title="Create project" className="text-xl text-slate-900 hover:text-2xl focus:font-semibold font-bold justify-content: space-between">CREATE PROJECT</Link>
             </div>
             <div>
-                <ul data-te-infinite-scroll-init className="overflow-y-scroll gap-6 max-w-screen md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                <ul data-te-infinite-scroll-init className="overflow-y-scroll gap-8 max-w-screen md:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                     {projects?.map((project: fill_project, idx: number) => {
                         const leaderUser = getUserInfo(project.leader_id);
                         return (
                             <li key={idx} className="m-4 flex flex-col justify-between relative rounded-xl bg-white text-black shadow-xl transition ease-in-out delay-100 hover:scale-105 duration-250">
-                                {/* <Link href={`/project/${project.id}`} title={project.name}> */}
-                                <Image src={project.image} alt={project.name} width={300} height={300} className="object-cover rounded-t-lg aspect-square mx-auto w-40 md:w-full"></Image>
-                                <div className="m-2 break-words">
-                                    <h1 className="text-center text-4xl break-words">{project.name}</h1>
-                                    <p className="line-clamp-5 mt-2 text-lg break-words">{project.description}</p>
-                                    <p className="italic font-light mt-8 p-2 text-sm break-words">
-                                        {leaderUser ? (leaderUser.email) : ('N/A')}
-                                    </p>
-                                </div>
-                                <div className={`rounded-b-lg ${getStatus(project.status)} w-auto h-10 md:h-12 lg:h-16 mb-0`}></div>
-                                {/* </Link> */}
+                                <Link href={`/project/${project.id}`} title={project.name}>
+                                    <Image src={project.image} alt={project.name} width={300} height={300} className="object-cover rounded-t-lg mx-auto w-full"></Image>
+                                    <div className="m-2 break-words">
+                                        <h1 className="text-center text-4xl break-words">{project.name}</h1>
+                                        <p className="line-clamp-5 mt-2 text-lg break-words">{project.description}</p>
+                                        <p className="italic font-light mt-8 p-2 text-sm break-words">
+                                            {leaderUser ? (leaderUser.email) : ('N/A')}
+                                        </p>
+                                    </div>
+                                    <div title={getTitleStatus(project.status)} className={`rounded-b-lg ${getStatus(project.status)} w-auto h-10 md:h-12 lg:h-16 mb-0`}></div>
+                                </Link>
                             </li>
                         );
                     })}
---
+++ frontend/pages/project/[id].tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import { fill_project, fill_user } from '../../model';
+import { format } from 'date-fns';
+import Image from 'next/image';
+import {getTitleStatus, getStatus} from '../project';
+
+
+const ProjectDetails: React.FC<{ project: fill_project; users: fill_user[] | undefined }> = ({ project, users }) => {
+    const getEmail = (member: number): string => {
+        const member_user = users?.find(user => user.id === member);
+        return member_user ? member_user.email : 'Member not found';
+    };
+    const formatDate = (inputDate: string): string => {
+        const date = new Date(inputDate);
+        return format(date, 'dd MM yyyy');
+    };
+    return (
+        <div className="bg-white m-8 p-4">
+            <div className="h-auto p-2 m-4 max-w-screen break-words gap-6 max-w-screen md:grid md:grid-cols-2">
+                <div>
+                    <h1 className="text-5xl m-8 text-slate-900">{project.name}</h1>
+                    <h1 className="text-2xl text-slate-900">{formatDate(project.created_at)}</h1>
+                </div>
+                <Image src={project.image} alt={project.name} width={300} height={300} className="object-cover rounded-lg mr-4"></Image>
+            </div>
+            <p className="mt-2 text-2xl break-words pb-4 ml-4">{project.description}</p>
+            <div className="h-auto p-2 m-4 max-w-screen break-words gap-6 max-w-screen md:grid md:grid-cols-2">
+                <ul className="list-disc">
+                    <h3 className="ml-4">Members:</h3>
+                    {project.members_id.map((member: number, idx: number) => (
+                        <li key={idx} className="ml-8">
+                            <p className="italic font-light text-lg break-words">{getEmail(member)}</p>
+                        </li>
+                    ))}
+                </ul>
+                <div title={getTitleStatus(project.status)} className={`rounded-full text-center inline-block align-middle ${getStatus(project.status)} h-10 md:h-12 lg:h-16 mb-0`}></div>
+            </div>
+        </div>
+    );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+    const projectId = params?.id as string;
+    try {
+        const projectRes = await fetch(`http://localhost:3000/projects/${projectId}`, { method: 'GET' });
+        const usersRes = await fetch('http://localhost:3000/users/', { method: 'GET' });
+        
+        if (!projectRes.ok || !usersRes.ok) {
+            throw new Error('Failed to fetch project or users');
+        }
+
+        const project: fill_project = await projectRes.json();
+        const users: fill_user[] = await usersRes.json();
+        return {
+            props: { project, users },
+        };
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return {
+            notFound: true,
+        };
+    }
+};
+
+export default ProjectDetails;
