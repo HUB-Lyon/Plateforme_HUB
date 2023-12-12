cc911dc89dff69f9ee19e7f718de7f6890c827b2
Nishidara <elias.abassi@epitech.eu>
2023-12-12 11:31:47 +0100

    [ADD] add pagination + test line clamp

---
+++ frontend/config.js
@@ -0,0 +1,5 @@
+const API_URL = process.env.NEXT_PUBLIC_DEBUG === 'true' ? 'apiUrl_hub' : 'http://localhost:3000';
+
+export {
+    API_URL,
+};
--- frontend/pages/inventory.tsx
+++ frontend/pages/inventory.tsx
@@ -1,40 +1,68 @@
-import { FC, useEffect, useState } from 'react';
-import InventoryLayout from './component/inventory_layout';
+import { FC, ReactNode, useState, useEffect } from 'react';
+import { API_URL } from '../config';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-interface InventoryItem {
-  id: number;
-  name: string;
-  quantity: number;
-  description: string;
+interface InventoryLayoutProps {
+    children: ReactNode;
 }
 
-const InventoryPage: FC = () => {
-    const [inventoryData, setInventoryData] = useState<InventoryItem[]>([]);
+const InventoryLayout: FC<InventoryLayoutProps> = ({ children }) => {
+    return (
+        <div className="container mx-auto mt-8">
+            <h1 className="text-4xl font-bold mb-4">Inventory</h1>
+            <div className="overflow-x-auto">{children}</div>
+        </div>
+    );
+};
+
+interface InventoryItem {
+    id: number;
+    name: string;
+    description: string;
+    quantity: number;
+}
+
+interface InventoryPageProps {
+    inventoryData: InventoryItem[];
+}
+
+const InventoryPage: FC<InventoryPageProps> = ({ inventoryData }) => {
+    const itemsPerPage = 6;
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = inventoryData.slice(indexOfFirstItem, indexOfLastItem);
+
+    const totalPages = Math.ceil(inventoryData.length / itemsPerPage);
 
     useEffect(() => {
-        const fetchInventoryData = async () => {
-            try {
-                const response = await fetch('http://localhost:3000/inventory', {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
+        // Update URL with page parameter
+        window.history.replaceState({}, '', `/inventory?page=${currentPage}`);
+    }, [currentPage]);
 
-                const data: InventoryItem[] = await response.json();
-                console.log(data);
-                setInventoryData(data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
+    const handlePageClick = (page: number) => {
+        setCurrentPage(page);
+    };
 
-        fetchInventoryData();
-    }, []);
+    const renderPageNumbers = () => {
+        const pageNumbers = [];
+        for (let i = 1; i <= totalPages; i++) {
+            pageNumbers.push(
+                <li key={i} className={currentPage === i ? 'active' : ''}>
+                    <a href={`?page=${i}`} onClick={(e) => { e.preventDefault(); handlePageClick(i); }}>
+                        {i}
+                    </a>
+                </li>
+            );
+        }
+        return pageNumbers;
+    };
 
     return (
         <InventoryLayout>
-            <table className="min-w-full bg-white border border-gray-300">
+            <table className="min-w-full bg-white border border-gray-300 mb-4">
                 <thead>
                     <tr>
                         <th className="py-3 px-6 bg-gray-300 text-center">ID</th>
@@ -44,8 +72,8 @@ const InventoryPage: FC = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {inventoryData.map((item) => (
-                        <tr key={item.id}>
+                    {currentItems.map((item) => (
+                        <tr key={item.id} className="h-20 overflow-hidden">
                             <td className="py-2 px-4 border-b text-center">{item.id}</td>
                             <td className="py-2 px-4 border-b text-center">{item.name}</td>
                             <td className="py-2 px-4 border-b text-center">{item.description}</td>
@@ -54,8 +82,66 @@ const InventoryPage: FC = () => {
                     ))}
                 </tbody>
             </table>
+            <ul className="pagination flex gap-2">
+                <li className={currentPage === 1 ? 'disabled' : ''}>
+                    <a
+                        href={`?page=${currentPage - 1}`}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            if (currentPage > 1) {
+                                handlePageClick(currentPage - 1);
+                            }
+                        }}
+                    >
+                        &laquo; Prev
+                    </a>
+                </li>
+                {renderPageNumbers()}
+                <li className={currentPage === totalPages ? 'disabled' : ''}>
+                    <a
+                        href={`?page=${currentPage + 1}`}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            if (currentPage < totalPages) {
+                                handlePageClick(currentPage + 1);
+                            }
+                        }}
+                    >
+                        Next &raquo;
+                    </a>
+                </li>
+            </ul>
         </InventoryLayout>
     );
+};
+
+
+export const getServerSideProps = async () => {
+    try {
+        const response = await fetch(`${API_URL}/inventory`);
+        const data: InventoryItem[] = await response.json();
+
+        return {
+            props: {
+                inventoryData: data,
+            },
+        };
+    } catch (error) {
+        toast.error(`An error occurred: ${error}`, {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: false,
+        });
+
+        return {
+            props: {
+                inventoryData: [],
+            },
+        };
+    }
 };
 
 export default InventoryPage;
