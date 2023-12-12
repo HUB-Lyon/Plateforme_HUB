19b899c00504b8b095264ef265fd52d9f25bf1af
tristan.calard <tristan.calard@epitech.eu>
2023-12-12 14:53:32 +0100

    [fix] delete button design

--- frontend/pages/article.tsx
+++ frontend/pages/article.tsx
@@ -5,6 +5,10 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import dynamic from 'next/dynamic';
 import 'react-quill/dist/quill.snow.css';
+import {
+    TrashIcon,
+    PlusIcon,
+} from '@heroicons/react/24/outline';
 
 const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
@@ -15,7 +19,7 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 800,
+    width: '0.8',
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -74,7 +78,8 @@ const Article: React.FC<ArticleProps> = ({ articlesData }) => {
     return (
         <div>
             <h1 id="openModal" className="text-7xl text-center mb-6 font-bold">Articles</h1>
-            <button onClick={handleOpen} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-8" >Add Article</button>
+            <button onClick={handleOpen} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded ml-8" ><PlusIcon className="lg:w-9 lg:h-9 md:w-7 md:h-7 sm:w-5 sm:h-5 w-5 h-5 inline-block"/> Add
+            </button>
             <Modal
                 open={open}
                 onClose={handleClose}
@@ -94,27 +99,27 @@ const Article: React.FC<ArticleProps> = ({ articlesData }) => {
                             setOpen(false);
                             update('');
                         }}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-8" >Add Article</button>
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6" >Add Article</button>
                 </Box>
             </Modal>
             <div className="markdown flex flex-col items-center gap-y-10 mb-10">
                 {articlesData.map(({ content, frontmatter, id}, index) => (
-                    <div key={index} className="markdown">
+                    <div key={index} className="group markdown">
                         <h1>{frontmatter.title}</h1>
                         <p>{frontmatter.date}</p>
-                        <button
-                            onClick={() => {
-                                deleteArticle(id);
-                            }}
-                            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                            Delete Article
-                        </button>
-                        <div className="flex-box w-full md:px-8 px-4 md:py-8 py-4">
+                        <div className="flex-box w-full md:px-8 px-4 md:py-8 py-4 relative">
                             <MarkdownView
                                 markdown={content}
                                 options={{ tables: true, emoji: true }}
                                 flavor="github"
                             />
+                            <button
+                                onClick={() => {
+                                    deleteArticle(id);
+                                }}
+                                className="py-2 px-4 rounded absolute right-0 top-0">
+                                <TrashIcon className="group-hover:text-red-500 lg:text-white xl:text-white text-red-500 lg:w-9 lg:h-9 md:w-7 md:h-7 sm:w-5 sm:h-5 w-5 h-5"/>
+                            </button>
                         </div>
                     </div>
                 ))}
