44832c7d5ad3b35e4eab9ea4fd102a5fe861daca
Mortua <laolou.laforest-ravon@epitech.eu>
2023-12-13 16:54:42 +0100

    [fix] : hover contrast-125

--- frontend/pages/project.tsx
+++ frontend/pages/project.tsx
@@ -58,7 +58,7 @@ const Project: React.FC<{ projects: Project[], users: User[] }> = ({ projects, u
                     {projects.map((project: Project) => {
                         const leaderUser = getUserInfo(project.leaderId);
                         return (
-                            <li key={project.id} className={`m-2 flex flex-col justify-between relative rounded-xl bg-white text-black shadow-xl transition ease-in-out delay-100 hover:scale-105 duration-250 border-b-8 ${getStatus(project.status)}`}>
+                            <li key={project.id} className={`m-2 hover:filter hover:contrast-125 flex flex-col justify-between relative rounded-xl bg-white text-black shadow-xl transition ease-in-out delay-100 hover:scale-105 duration-250 border-b-8 ${getStatus(project.status)}`}>
                                 <Link href={`/project/${project.id}`} title={`${getTitleStatus(project.status)}\n${project.name}`}>
                                     <Image src={project.image} alt="" width={300} height={300} className="object-cover rounded-t-lg mx-auto w-full"/>
                                     <div className="m-2 break-words">
