716da93c07130efa9501f239dc393937be034867
debize <lucas.debize@epitech.eu>
2023-12-13 15:14:54 +0100

    [fix] build issue

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { MsalProvider } from '@azure/msal-react';
 import { PublicClientApplication } from '@azure/msal-browser';
-import LoginPage from './component/login';
-import RootLayout from './component/RootLayout';
-import { msalConfig } from './component/msalConfig';
+import LoginPage from './../components/login';
+import RootLayout from './../components/RootLayout';
+import { msalConfig } from './../components/msalConfig';
 import '../styles/globals.css';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
