5d3041ffca161bd2b5cad1a0599318d9a58f47a9
Mortua <laolou.laforest-ravon@epitech.eu>
2023-12-14 10:43:09 +0100

    [fix] : toaster on project page; next.config.ts is empty; line under sidebaropen

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                hostname: 'loremflickr.com',
-            },
-            {
-                hostname: 'picsum.photos',
-            },
-        ],
-    },
 };
 
 module.exports = nextConfig;
