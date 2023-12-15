3af189141ef0447bb64d6cf7d3758c8be89d6c9d
tristan.calard <tristan.calard@epitech.eu>
2023-12-15 16:06:21 +0100

    [feat] headless with component

--- frontend/pages/article.tsx
+++ frontend/pages/article.tsx
@@ -1,19 +1,15 @@
-import React, { useState, useEffect, Fragment} from 'react';
+import React, { useState, useEffect} from 'react';
 import matter from 'gray-matter';
 import MarkdownView from 'react-showdown';
-import dynamic from 'next/dynamic';
-import 'react-quill/dist/quill.snow.css';
 import { API_URL } from '../config.js';
-import { Dialog, Transition} from '@headlessui/react';
 import {
     TrashIcon,
     PlusIcon,
     PencilIcon,
 } from '@heroicons/react/24/outline';
+import DialogComponent from '../components/dialog';
 
 const admin = true;
-
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
 const STORAGE_KEY = 'ARTICLE_POST';
 const STORAGE_KEY2 = 'ARTICLE_PATCH';
@@ -100,81 +96,28 @@ const Article: React.FC<ArticleProps> = ({ articlesData: initialArticlesData }) 
 
     const [articlesData, setArticlesData] = useState(initialArticlesData);
 
+    async function addaction (value: string) {
+        const response = await addArticle(`Article_${new Date().toISOString()}`, value);
+        const newarticle = await response.json();
+        setArticlesData(old => [...old, newarticle]);
+        setOpenAdd(false);
+        updateAdd('');
+    }
+
+    async function patchaction (value: string, id: number, name: string) {
+        const response = await patchArticle(id, name, value);
+        const updatedarticle = await response.json();
+        setArticlesData(old => old.map(article => (article.id === id ? updatedarticle : article)));
+        setOpenPatch(false);
+        updatePatch('');
+    }
+
     return (
         <div>
             <h1 id="openModal" className="text-4xl text-center font-bold">Articles</h1>
             { admin && (<button onClick={handleOpenAdd} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded ml-8 flex items-center" ><PlusIcon className="lg:w-9 lg:h-9 md:w-7 md:h-7 sm:w-5 sm:h-5 w-5 h-5 inline-block"/> Add
             </button>)}
-            <Transition show={openAdd} as={Fragment}>
-                <Dialog onClose={handleCloseAdd} className="fixed z-10 inset-0 overflow-y-auto">
-                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0"
-                            enterTo="opacity-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                        >
-                            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                        </Transition.Child>
-                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-                        &#8203;
-                        </span>
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                            enterTo="opacity-100 translate-y-0 sm:scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        >
-                            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-2xl sm:w-full">
-                                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                    <div className="sm:flex sm:items-start">
-                                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                            <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                                            Add Article
-                                            </Dialog.Title>
-                                            <div className="mt-2 w-full">
-                                                <ReactQuill
-                                                    theme="snow"
-                                                    value={valueAdd}
-                                                    onChange={updateAdd}
-                                                    className="h-full w-full text-black"
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                                    <button
-                                        onClick={async () => {
-                                            const response = await addArticle(`Article_${new Date().toISOString()}`, valueAdd);
-                                            const newarticle = await response.json();
-                                            setArticlesData(old => [...old, newarticle]);
-                                            setOpenAdd(false);
-                                            updateAdd('');
-                                        }}
-                                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                    >
-                                    Add Article
-                                    </button>
-                                    <button
-                                        onClick={handleCloseAdd}
-                                        type="button"
-                                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                                    >
-                                    Cancel
-                                    </button>
-                                </div>
-                            </div>
-                        </Transition.Child>
-                    </div>
-                </Dialog>
-            </Transition>
+            <DialogComponent open={openAdd} title='Add Article' onClose={handleCloseAdd} value={valueAdd} update={updateAdd} action={() => addaction(valueAdd)}/>
             <div className="markdown flex flex-col items-center gap-y-10 mb-10">
                 {articlesData.map(({ content, id, name}, index) => (
                     <div key={index} className="group markdown">
@@ -197,76 +140,7 @@ const Article: React.FC<ArticleProps> = ({ articlesData: initialArticlesData }) 
                                 className="py-2 px-4 rounded absolute right-0 bottom-0">
                                 <PencilIcon className="group-hover:text-orange-500 lg:text-white xl:text-white text-orange-500 lg:w-9 lg:h-9 md:w-7 md:h-7 sm:w-5 sm:h-5 w-5 h-5"/>
                             </button>)}
-                            <Transition show={openPatch} as={Fragment}>
-                                <Dialog onClose={handleClosePatch} className="fixed z-10 inset-0 overflow-y-auto">
-                                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                                        <Transition.Child
-                                            as={Fragment}
-                                            enter="ease-out duration-300"
-                                            enterFrom="opacity-0"
-                                            enterTo="opacity-100"
-                                            leave="ease-in duration-200"
-                                            leaveFrom="opacity-100"
-                                            leaveTo="opacity-0"
-                                        >
-                                            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                                        </Transition.Child>
-                                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-                                        &#8203;
-                                        </span>
-                                        <Transition.Child
-                                            as={Fragment}
-                                            enter="ease-out duration-300"
-                                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                            enterTo="opacity-100 translate-y-0 sm:scale-100"
-                                            leave="ease-in duration-200"
-                                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                        >
-                                            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-2xl sm:w-full">
-                                                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                                    <div className="sm:flex sm:items-start">
-                                                        <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                                            <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                                                            Patch Article
-                                                            </Dialog.Title>
-                                                            <div className="mt-2 w-full">
-                                                                <ReactQuill
-                                                                    theme="snow"
-                                                                    value={valuePatch}
-                                                                    onChange={updatePatch}
-                                                                    className="h-full w-full text-black"
-                                                                />
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                                                    <button
-                                                        onClick={async () => {
-                                                            const response = await patchArticle(id, name, valuePatch);
-                                                            const updatedarticle = await response.json();
-                                                            setArticlesData(old => old.map(article => (article.id === id ? updatedarticle : article)));
-                                                            setOpenPatch(false);
-                                                            updatePatch('');
-                                                        }}
-                                                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-500 text-base font-medium text-white hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                                    >
-                                                    Edit Article
-                                                    </button>
-                                                    <button
-                                                        onClick={handleClosePatch}
-                                                        type="button"
-                                                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                                                    >
-                                                    Cancel
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </Transition.Child>
-                                    </div>
-                                </Dialog>
-                            </Transition>
+                            <DialogComponent open={openPatch} title='Edit Article' onClose={handleClosePatch} value={valuePatch} update={updatePatch} action={() => patchaction(valuePatch, id, name)}/>
                         </div>
                     </div>
                 ))}
