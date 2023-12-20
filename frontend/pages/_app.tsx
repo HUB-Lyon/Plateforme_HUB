f4e0f15f56ef5f06af35a84c8cb759d79a820ace
debize <lucas.debize@epitech.eu>
2023-12-20 11:58:37 +0100

    [fix] add toastcontainer back

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
 import { AuthenticatedTemplate, MsalProvider, UnauthenticatedTemplate } from '@azure/msal-react';
 import { PublicClientApplication } from '@azure/msal-browser';
 import LoginPage from '../components/login';
 import RootLayout from './../components/RootLayout';
 import { msalConfig } from './../components/msalConfig';
+import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
@@ -16,6 +18,14 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
                 <AuthenticatedTemplate>
                     <RootLayout>
                         <Component {...pageProps} />
+                        <ToastContainer 
+                            position= 'top-right'
+                            autoClose= {5000}
+                            hideProgressBar= {false}
+                            closeOnClick= {true}
+                            pauseOnHover= {true}
+                            draggable= {false}
+                        />
                     </RootLayout>
                 </AuthenticatedTemplate>
                 <UnauthenticatedTemplate>
